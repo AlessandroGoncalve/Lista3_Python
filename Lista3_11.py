662576b1605c8caf68ffe49817707b279a4133d2
AlessandroGoncalve <50560260+AlessandroGoncalve@users.noreply.github.com>
2020-09-01 13:48:19 -0300

    Update Lista3_11.py

--- Lista3_11.py
+++ Lista3_11.py
@@ -3,10 +3,7 @@
 n1 = int(input("Digite um número: "))
 n2 = int(input("Digite outro número: "))
 
-for i in range(n1 + 1, n2):
-        print(i)
-
-for i in range(n2 + 1, n1):
-        print(i)
-
-print("Soma dos números: ", i + i)
+for n in range(n1 + 1, n2) or range(n2 + 1, n1):
+    print(n)
+    soma = soma + n
+print("A soma dos números que estão no intervalo é %s " % soma)
